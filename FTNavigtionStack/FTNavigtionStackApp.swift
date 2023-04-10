4411769dcc1fd0d3c29e7e44ca1a0a9f3012cb86
Akshay <akshay@fluidtouch.biz>
2023-04-10 17:21:12 +0530

    Initial Commit

---
+++ FTNavigtionStack/FTNavigtionStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FTNavigtionStackApp.swift
+//  FTNavigtionStack
+//
+//  Created by Akshay on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FTNavigtionStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
